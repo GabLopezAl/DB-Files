4ead3f3f575d44cf6753326982380c8cc592dee3
GabrielHome <lopezalvaradoa749@gmail.com>
2024-03-01 21:49:44 -0600

    archivoEjemplo

---
+++ sumaDosNum.py
@@ -0,0 +1,10 @@
+"""
+Suma de dos numeros introducidos por el usuario
+"""
+
+num1 = int(input('Write a number: '))
+num2 = int(input('Write another number: '))
+
+resultado = num1 + num2
+
+print(f'La suma de {num1} y {num2} es: {resultado}')
